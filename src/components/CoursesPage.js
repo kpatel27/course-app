da9efe6712e97f522d7d5ae702d4d82c20de03d5
Krunal Patel <kppatel727@gmail.com>
2020-06-21 03:32:29 -0700

    added handleFormSubmit

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import * as courseActions from '../redux/actions/courseActions';
 
-export default class CoursesPage extends Component {
+class CoursesPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      course: {
+        title: '',
+      },
+    };
+  }
+
+  handleChange = (e) => {
+    const course = { ...this.state.course, title: e.target.value };
+    this.setState({ course });
+  };
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    this.props.dispatch(courseActions.createCourse(this.state.course));
+  };
+
+  displayCourses = () => {
+    return this.props.courses.map((course, idx) => (
+      <div key={idx}>{course.title}</div>
+    ));
+  };
+
   render() {
     return (
-      <div className='App-header'>
-        <h1>Courses</h1>
-      </div>
+      <form onSubmit={this.handleFormSubmit}>
+        <h2>Courses</h2>
+        <h3>Add New Course</h3>
+        <input
+          type='text'
+          value={this.state.course.title}
+          onChange={this.handleChange}
+        />
+        <input type='submit' value='Add' />
+        {this.displayCourses()}
+      </form>
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    courses: state.courses,
+  };
+};
+
+CoursesPage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps)(CoursesPage);
